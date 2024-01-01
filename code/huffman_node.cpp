0601ee7309ddfabf10f08c5b71b72ba2db4ab51f
marlanbranda <emrek1999@gmail.com>
2024-01-01 23:52:43 +0300

    head-start, basic organization

---
+++ code/huffman_node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emrek on 23.12.2023.
+//
+
+#include "huffman_node.h"
---
+++ code/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // read the file
+    std::cout << "emre" << std::endl;
+
+
+}
