003c897baccaa9490f1c8bd258bc66cb3e8786e8
marlanbranda <emrek1999@gmail.com>
2024-01-04 23:35:35 +0300

    frequency table functions defined

--- code/freqTable.cpp
+++ code/freqTable.cpp
@@ -1,19 +1,60 @@
 #include "freqTable.h"
 
+#include <iostream>
+#include <fstream>
+
 freqTable::freqTable(char *data_stream, int size) {
-    this->unique_count = this->count_unique_symbols(data_stream);
-    this->unique_array = this->create_unique_array(data_stream, size);
-    this->unique_freq = this->create_frequency_array(data_stream,  size);
-}
-
-int freqTable::count_unique_symbols(char *data_stream) {
+    this->unique_freq = create_frequency_array(data_stream, size);
+    this->unique_count = count_unique_symbols();
+    this->unique_array = create_unique_array(data_stream);
 
 }
 
-char *freqTable::create_unique_array(char *data_stream, int size) {
-    return nullptr;
-}
+int** freqTable::create_frequency_array(char *data_stream, int size) {
 
-int *freqTable::create_frequency_array(char *data_stream, int size) {
-    return nullptr;
+    int** symbols_array = new int*[256];
+    for(int i=0; i<256; i++){
+        symbols_array[i] = new int[2];
+        symbols_array[i][0] = i; // 0 index for symbol, 1 index is freq
+        symbols_array[i][1] = 0;
+    }
+
+    for(int i=0; i<size; i++){
+        auto ch = (unsigned char) data_stream[i];
+        int num = (int) ch;
+        symbols_array[num][1] += 1;
+    }
+
+    return symbols_array;
 };
+
+int freqTable::count_unique_symbols() const {
+    int num = 0;
+    int** symbols_array = this->unique_freq;
+
+    for(int i=0; i<256; i++){
+        if( symbols_array[i][1] >= 1)
+            num++;
+    }
+
+    return num;
+}
+
+char* freqTable::create_unique_array(char *data_stream) {
+    char* unique_chars = new char[this->unique_count];
+    int** symbols_array = this->unique_freq;
+    int num = 0;
+
+    for(int i=0; i<256; i++){
+        if( symbols_array[i][1] >= 1) {
+            auto ch = (unsigned char) symbols_array[i][0];
+            unique_chars[num] = (int) ch;
+        }
+    }
+
+    return unique_chars;
+}
+
+freqTable::~freqTable() = default;
+
+
--- code/freqTable.h
+++ code/freqTable.h
@@ -6,13 +6,14 @@ class freqTable {
 public:
     int unique_count; // how many unique char is used in data_stream
     char* unique_array; // unique symbols
-    int* unique_freq; // frequency of each symbol in same order with unique_array
+    int** unique_freq; // frequency of each symbol in same order with unique_array
 
     freqTable(char* data_stream, int size);
+    ~freqTable();
 
-    int count_unique_symbols(char* data_stream);
-    char* create_unique_array(char* data_stream, int size);
-    int* create_frequency_array(char* data_stream, int size);
+    int** create_frequency_array(char* data_stream, int size);
+    int count_unique_symbols() const;
+    char* create_unique_array(char* data_stream);
 };
 
 
