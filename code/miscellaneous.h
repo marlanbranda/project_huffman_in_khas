281ad102d9c0d7d11906b7032050def652d2b544
marlanbranda <emrek1999@gmail.com>
2024-01-07 00:19:30 +0300

    - misc functions for testing

--- code/miscellaneous.h
+++ code/miscellaneous.h
@@ -1,5 +1,12 @@
 #include <stdexcept>
+#include <random>
+
+#include "treeNode.h"
 
 int ASCII_char_to_int(char ch);
 
 int ASCII_str_to_int(std::string &str);
+
+linkedNode* create_dummy_linked_node();
+
+void print_linked_node(linkedNode* currLN);
